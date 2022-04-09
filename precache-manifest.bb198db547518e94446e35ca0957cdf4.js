84060b177418b4e63c939e86236f33c4f3cf7208
Redi <redi@hpcnt.com>
2022-04-09 13:13:37 +0900

    Updates

---
+++ precache-manifest.bb198db547518e94446e35ca0957cdf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33555bcff9d454b5272a9bfad02de002",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "8ac989ee61288c6f5f15",
+    "url": "/react-calculator/static/css/2.81187608.chunk.css"
+  },
+  {
+    "revision": "78321b755ca18b48e80b",
+    "url": "/react-calculator/static/css/main.a88b9e4e.chunk.css"
+  },
+  {
+    "revision": "8ac989ee61288c6f5f15",
+    "url": "/react-calculator/static/js/2.971a1cc1.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-calculator/static/js/2.971a1cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78321b755ca18b48e80b",
+    "url": "/react-calculator/static/js/main.69fd6e40.chunk.js"
+  },
+  {
+    "revision": "1274cb5e1d827d69054c",
+    "url": "/react-calculator/static/js/runtime-main.c8e7dff3.js"
+  }
+]);
